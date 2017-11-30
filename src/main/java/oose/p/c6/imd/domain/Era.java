bb78740623b72ed652199d9691c432266d5754a5
Lennard Rottjers <LL.Rottjers@student.han.nl>
2017-11-30 14:45:16 +0100

    pojos gegenerate

--- src/main/java/oose/p/c6/imd/domain/Era.java
+++ src/main/java/oose/p/c6/imd/domain/Era.java
@@ -3,5 +3,16 @@ package oose.p.c6.imd.domain;
 public class Era extends Model {
     public String name;
 
+    public Era(int id, String name) {
+        super(id);
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
