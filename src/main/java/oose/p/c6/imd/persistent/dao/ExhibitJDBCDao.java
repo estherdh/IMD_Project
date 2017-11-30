104cf6b7bbbced502fa7c5e8aabcafdb9568eee2
Lennard Rottjers <LL.Rottjers@student.han.nl>
2017-11-30 15:18:32 +0100

    DAO geimplementeerd

--- src/main/java/oose/p/c6/imd/persistent/dao/ExhibitJDBCDao.java
+++ src/main/java/oose/p/c6/imd/persistent/dao/ExhibitJDBCDao.java
@@ -1,6 +1,8 @@
 package oose.p.c6.imd.persistent.dao;
 
+import oose.p.c6.imd.domain.Era;
 import oose.p.c6.imd.domain.Exhibit;
+import oose.p.c6.imd.domain.Museum;
 import oose.p.c6.imd.domain.User;
 import oose.p.c6.imd.persistent.ConnectMySQL;
 
@@ -10,6 +12,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -51,6 +54,87 @@ public class ExhibitJDBCDao implements IExhibitDao {
     @Override
     public List<Exhibit> list(User user) {
         return null;
+    }
+
+    @Override
+    public Era findEra(User user, int eraId) {
+        Connection connection = ConnectMySQL.getInstance().getConnection();
+        ResultSet rs = null;
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT name FROM EraLanguage el WHERE el.EraId = ? AND LanguageId = COALESCE((SELECT el2.LanguageId FROM EraLanguage el2 WHERE el2.LanguageId = ? AND el2.EraId = el.EraId), 1)");
+            ps.setInt(1, eraId);
+            ps.setInt(2, user.getLanguageId());
+            rs = ps.executeQuery();
+            Era e = null;
+            if(rs.next()){
+                e = new Era(rs.getInt("EraId"), rs.getString("name"));
+            }
+            connection.close();
+            return e;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            return null;
+        }
+    }
+
+    @Override
+    public List<Era> listEra(User user) {
+        Connection connection = ConnectMySQL.getInstance().getConnection();
+        ResultSet rs = null;
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT name FROM EraLanguage el WHERE el.EraId = ? AND LanguageId = COALESCE((SELECT el2.LanguageId FROM EraLanguage el2 WHERE el2.LanguageId = ? AND el2.EraId = el.EraId), 1)");
+            rs = ps.executeQuery();
+            List<Era> list = new ArrayList<>();
+            if(rs.next()){
+                Era e = new Era(rs.getInt("EraId"), rs.getString("name"));
+                list.add(e);
+            }
+            connection.close();
+            return list;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            return null;
+        }
+    }
+
+    @Override
+    public Museum findMuseum(int museumId) {
+        Connection connection = ConnectMySQL.getInstance().getConnection();
+        ResultSet rs = null;
+        try {
+            PreparedStatement ps = connection.prepareStatement("Select * FROM Museum WHERE museumId = ?");
+            ps.setInt(1, museumId);
+            rs = ps.executeQuery();
+            Museum m = null;
+            if(rs.next()){
+                m = new Museum(rs.getInt("MuseumId"), rs.getString("MuseumName"), rs.getString("Website"), rs.getString("Region"));
+            }
+            connection.close();
+            return m;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            return null;
+        }
+    }
+
+    @Override
+    public List<Museum> listMuseums() {
+        Connection connection = ConnectMySQL.getInstance().getConnection();
+        ResultSet rs = null;
+        try {
+            PreparedStatement ps = connection.prepareStatement("Select * FROM Museum");
+            rs = ps.executeQuery();
+            List<Museum> list = new ArrayList<>();
+            if(rs.next()){
+                Museum m = new Museum(rs.getInt("MuseumId"), rs.getString("MuseumName"), rs.getString("Website"), rs.getString("Region"));
+                list.add(m);
+            }
+            connection.close();
+            return list;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+            return null;
+        }
     }
 
 
